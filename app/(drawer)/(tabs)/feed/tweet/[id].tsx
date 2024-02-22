bda6677857b3531e417eba33fb5b273eaffe9b04
Josh Ready <jready9767@gmail.com>
2024-02-22 17:29:02 -0600

    Fetch ONE tweet

--- app/(drawer)/(tabs)/feed/tweet/[id].tsx
+++ app/(drawer)/(tabs)/feed/tweet/[id].tsx
@@ -1,16 +1,27 @@
-import { Text } from 'react-native';
-import tweets from '../../../../../assets/data/tweets';
+import { ActivityIndicator, Text } from 'react-native';
 import Tweet from '../../../../../components/Tweet';
 import { useGlobalSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import { getTweet } from '../../../../../lib/api/tweets';
 
 export default function TweetScreen() {
     const { id } = useGlobalSearchParams();
 
-    const tweet = tweets.find((t) => t.id == id);
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['tweet', id],
+        queryFn: () => getTweet(id as string)
+    })
 
-    if (!tweet) {
+    // ** Below is where you need to find tweets through dummy data. No longer needed with function above pulling it from live data **
+    // const tweet = tweets.find((t) => t.id == id);
+
+    if (isLoading) {
+        return <ActivityIndicator />;
+    }
+
+    if (error) {
         return <Text>Tweet {id} not found!</Text>
     }
 
-    return <Tweet tweet={tweet} />;
+    return <Tweet tweet={data} />;
 }
